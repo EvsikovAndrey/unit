049257275e3e5c3114f10231aa5040f706dfd913
EvsikovAndrey <104692334+EvsikovAndrey@users.noreply.github.com>
2023-04-18 19:41:43 +0300

    Add files via upload

---
+++ js/clients-carousel.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    // $("#clients-slider").owlCarousel({
+    //     items: 1
+        
+    // });
+    const owl = $('#clients-slider');
+    owl.owlCarousel({
+        items: 1,
+        loop: true
+    });
+
+    const prev = $('#sliderPrev');
+    const next = $('#sliderNext');
+    prev.click(function(){
+        owl.trigger('prev.owl.carousel')
+    })
+
+    next.click(function(){
+        owl.trigger('next.owl.carousel')
+    })
+
+  });
+
---
+++ js/search.js
@@ -0,0 +1,10 @@
+const searchForm = document.querySelector('.search');
+const searchButton = document.querySelector('.search__button');
+
+
+searchButton.addEventListener('click', function(e){
+    
+        e.preventDefault();
+        searchForm.classList.toggle('search--visible')
+     
+})
